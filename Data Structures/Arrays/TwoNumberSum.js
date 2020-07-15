2bfcc42c9908d1b4482ed43d8001de760330aa38
Jake Joseph <jakejosephcs@gmail.com>
2020-07-14 20:31:01 -0400

    Finish TwoNumberSum implementation

--- Data Structures/Arrays/TwoNumberSum.js
+++ Data Structures/Arrays/TwoNumberSum.js
@@ -32,7 +32,37 @@ function twoNumberSum(array, targetSum) {
   return []
 }
 
-// Solution 2: O(n) time | O(1) space
+// Solution 2: O(n) time | O(n) space
+function twoNumberSum(array, targetSum) {
+  const nums = {};
+  for (const num of array) {
+    const potentialMatch = targetSum - num;
+    if (potentialMatch in nums) {
+      return [potentialMatch, num];
+    } else {
+      nums[num] = true;
+    }
+  }
+  return [];
+}
 
 
 // Solution 3: O(nlong(n)) time | O(1) space
+function twoNumberSum(array, targetSum) {
+  array.sort((a,b) => a - b)
+  let left = 0;
+  let right = array.length - 1;
+  while (left < right) {
+    let currentSum = array[left] + array[right];
+    if (currentSum === targetSum) {
+      return [array[left], array[right]];
+    }
+    if (currentSum < targetSum) {
+      left++;
+    }
+    if (currentSum > targetSum) {
+      right--;
+    }
+  }
+  return [];
+}
