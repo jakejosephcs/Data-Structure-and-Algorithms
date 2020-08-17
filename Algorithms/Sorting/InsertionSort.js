1e68d8cf0e7422a1b12ccd90462281b3ccb9fe36
Jake Joseph <jakejosephcs@gmail.com>
2020-08-16 21:38:34 -0400

    Finish insertionSort problem

--- Algorithms/Sorting/InsertionSort.js
+++ Algorithms/Sorting/InsertionSort.js
@@ -1,20 +1,11 @@
 // O(n^2) time | O(1) space
-
-const numArray = [2, 8, 5, 3, 9, 4, 1]
-
 function insertionSort(array) {
-  for (let i=1; i<array.length; i++) {
-    let current = array[i];
-    let j = i-1;
-    while (j >= 0 && array[j] > current) {
-      array[j+1] = array[j];
+  for (let i = 1; i < array.length; i++) {
+    let j = i;
+    while (j > 0 && array[j] < array[j - 1]) {
+      [array[j - 1], array[j]] = [array[j], array[j - 1]];
       j--;
     }
-    array[j+1] = current
   }
+  return array;
 }
-
-insertionSort(numArray)
-
-
-
