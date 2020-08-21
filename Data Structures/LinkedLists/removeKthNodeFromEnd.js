423091c2a8ba08996499ffce341547b546a43db6
Jake Joseph <jakejosephcs@gmail.com>
2020-08-21 19:53:27 -0400

    Finish removeKthNodeFromEnd problem

---
+++ Data Structures/LinkedLists/removeKthNodeFromEnd.js
@@ -0,0 +1,33 @@
+// This is an input class. Do not edit.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+// O(n) time | O(1) space
+function removeKthNodeFromEnd(head, k) {
+  let counter = 1;
+  let first = head;
+  let second = head;
+
+  while (counter <= k) {
+    second = second.next;
+    counter++;
+  }
+
+  if (second === null) {
+    head.value = head.next.value;
+    head.next = head.next.next;
+    return;
+  }
+
+  while (second.next !== null) {
+    first = first.next;
+    second = second.next;
+  }
+
+  first.next = first.next.next;
+}
+
