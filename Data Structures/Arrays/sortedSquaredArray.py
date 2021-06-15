6c3eb786ba0adeb1d7b2300b747a6b368d3f53c2
Jake Joseph <jakejosephcs@gmail.com>
2021-06-15 15:11:31 -0400

    Add optimal solution

--- Data Structures/Arrays/sortedSquaredArray.py
+++ Data Structures/Arrays/sortedSquaredArray.py
@@ -7,7 +7,7 @@
 # input: array of integers
 # output: array of integers
 
-# Naive solutions
+# Naive solutions | time: O(nlogn) | space: O(1)
 def sortedSquaredArray(array):
     # Iterate through array and turn negatives to positives (in place)
     # Sort the array (in place)
@@ -16,10 +16,28 @@ def sortedSquaredArray(array):
     for i in range(len(array)):
         if array[i] < 0:
             array[i] = array[i] * -1
-    
+
     array.sort()
 
     for i in range(len(array)):
-      array[i] = array[i] ** 2
-    
+        array[i] = array[i] ** 2
+
     return array
+
+# Optimal solutions | time: O(n) | space: O(n)
+def sortedSquaredArray(array):
+    newArray = [None] * len(array)
+    arrayStartPointer = 0
+    arrayEndPointer = len(array) - 1
+    newArrayPointer = len(array) - 1
+
+    while newArrayPointer >= 0:
+        if abs(array[arrayStartPointer]) > abs(array[arrayEndPointer]):
+            newArray[newArrayPointer] = array[arrayStartPointer]**2
+            arrayStartPointer = arrayStartPointer + 1
+            newArrayPointer = newArrayPointer - 1
+        else:
+            newArray[newArrayPointer] = array[arrayEndPointer]**2
+            arrayEndPointer = arrayEndPointer - 1
+            newArrayPointer = newArrayPointer - 1
+    return newArray
