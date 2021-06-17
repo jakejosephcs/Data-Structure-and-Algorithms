38839088b331ebbaf7e9544e3786cb64bd003fe2
Jake Joseph <jakejosephcs@gmail.com>
2021-06-16 20:23:11 -0400

    Add naive solution

---
+++ Data Structures/Arrays/majorityElement.py
@@ -0,0 +1,19 @@
+######### PROBLEM: Leetcode 169. Majority Element ##########
+######### SOLUTION ##########
+# Naive solution
+# Time: O(n) where n is the size of the nums array
+# Space: O(n)
+def majorityElement(nums):
+    numFreq = dict()
+    for num in nums:
+        if numFreq.get(num) == None:
+            numFreq[num] = 1
+        else:
+            numFreq[num] += 1
+    majorityElement = None
+    majorityElementFreq = 0
+    for k, v in numFreq.items():
+        if v > majorityElementFreq:
+            majorityElement = k
+            majorityElementFreq = v
+    return majorityElement
