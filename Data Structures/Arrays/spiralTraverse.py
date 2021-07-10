668eb4c307f6e1ac5763c195d8205aa0dda3e348
Jake Joseph <jakejosephcs@gmail.com>
2021-07-09 20:21:46 -0400

    Add solution to spiralTraverse

--- Data Structures/Arrays/spiralTraverse.py
+++ Data Structures/Arrays/spiralTraverse.py
@@ -8,3 +8,39 @@
 # has been visited.
 
 ######### SOLUTION ##########
+# Optimal Solution
+# Time: O(n) where n is the size of the array
+# Space: O(n)
+def spiralTraverse(array):
+    finalArray = []
+    top = 0
+    bottom = len(array) - 1
+    left = 0
+    right = len(array[0]) - 1
+    size = len(array) * len(array[0])
+
+    while len(finalArray) < size:
+        for i in range(left, right + 1):
+            if len(finalArray) == size:
+                break
+            finalArray.append(array[top][i])
+        top = top + 1
+
+        for i in range(top, bottom + 1):
+            if len(finalArray) == size:
+                break
+            finalArray.append(array[i][right])
+        right = right - 1
+
+        for i in range(right, left - 1, -1):
+            if len(finalArray) == size:
+                break
+            finalArray.append(array[bottom][i])
+        bottom = bottom - 1
+
+        for i in range(bottom, top - 1, -1):
+            if len(finalArray) == size:
+                break
+            finalArray.append(array[i][left])
+        left = left + 1
+    return finalArray
