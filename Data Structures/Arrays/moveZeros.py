9f671b8567a44cc54903686d243c87219757f830
Jake Joseph <jakejosephcs@gmail.com>
2021-07-14 19:31:15 -0400

    Add solution to moveZeros

---
+++ Data Structures/Arrays/moveZeros.py
@@ -0,0 +1,25 @@
+######### PROBLEM: Leetcode 283. Move Zeroes ##########
+######### SOLUTION ##########
+# Naive solution
+# Time: O(n^2) where n is the size of the nums array
+# Space: O(1)
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        if len(nums) == 1:
+            return nums
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                continue
+            else:
+                for j in range(i+1, len(nums)):
+                    if nums[j] == 0:
+                        continue
+                    else:
+                        nums[i], nums[j] = nums[j], nums[i]
+                        break
+        return nums
