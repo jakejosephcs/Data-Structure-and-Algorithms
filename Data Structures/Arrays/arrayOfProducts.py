3188ae6c1be822c9bd9a3d3d70d99c1d92802de8
Jake Joseph <jakejosephcs@gmail.com>
2021-07-13 21:14:24 -0400

    Add arrayOfProducts  problem statement

---
+++ Data Structures/Arrays/arrayOfProducts.py
@@ -0,0 +1,13 @@
+######### PROBLEM ##########
+  # Write a function that takes in a non-empty array of integers and returns an
+  # array of the same length, where each element in the output array is equal to
+  # the product of every other number in the input array.
+  # In other words, the value at output[i] is equal to the product of
+  # every number in the input array other than input[i].
+  # Note that you're expected to solve this problem without using division.
+
+######### SOLUTION ##########
+# Naive solution
+def arrayOfProducts(array):
+    # Write your code here.
+    pass
