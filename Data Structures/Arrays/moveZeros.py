fc3dd6924387756c2f647884eda094d1afe55f58
Jake Joseph <jakejosephcs@gmail.com>
2021-07-14 20:31:27 -0400

    Add optimal solution for moveZeros

--- Data Structures/Arrays/moveZeros.py
+++ Data Structures/Arrays/moveZeros.py
@@ -1,9 +1,9 @@
 ######### PROBLEM: Leetcode 283. Move Zeroes ##########
+
 ######### SOLUTION ##########
 # Naive solution
 # Time: O(n^2) where n is the size of the nums array
 # Space: O(1)
-
 class Solution(object):
     def moveZeroes(self, nums):
         """
@@ -22,4 +22,25 @@ class Solution(object):
                     else:
                         nums[i], nums[j] = nums[j], nums[i]
                         break
+        return nums
+
+######### SOLUTION ##########
+# Optimal solution
+# Time: O(n) where n is the size of the nums array
+# Space: O(1)
+
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        nonZeroPointer = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[nonZeroPointer] = nums[i]
+                nonZeroPointer += 1
+        for j in range(nonZeroPointer, len(nums)):
+            nums[j] = 0
         return nums
